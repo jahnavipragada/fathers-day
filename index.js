5125083669a42ada852237bac9cd879360c7af3d
jahnavipragada <65712625+jahnavipragada@users.noreply.github.com>
2020-06-20 19:40:27 +0530

    father's day surprise

---
+++ index.js
@@ -0,0 +1,32 @@
+
+var randomNumber1= (Math.floor(Math.random()*6));
+var randomNumber2= (Math.floor(Math.random()*5));
+var randomNumber3= (Math.floor(Math.random()*4));
+
+var randomImage1 = "circle-cropped ("+ randomNumber1 +").png" ;
+var randomImage2 = "img_" + randomNumber2 + ".jpg" ;
+
+var randomImageSource1 = "images/" + randomImage1;
+var randomImageSource2 = "images/" + randomImage2;
+
+var image1 = document.querySelectorAll("img")[0];
+var image2 = document.querySelectorAll("img")[1];
+
+image1.setAttribute("src", randomImageSource1);
+image2.setAttribute("src", randomImageSource2);
+
+console.log(randomNumber1);
+console.log(randomNumber2);
+
+if(randomNumber3===0){
+  document.querySelector("h1").innerHTML = "पिता दिवस की शुभकामना";
+}
+else if (randomNumber3===1){
+  document.querySelector("h1").innerHTML =  "పితృ దినోత్సవ శుభాకాంక్షలు";
+}
+else if (randomNumber3===2){
+  document.querySelector("h1").innerHTML =  "Happy Father,s Day";
+}
+else{
+  document.querySelector("h1").innerHTML =" இனிய தந்தையர் தினம்";
+}
